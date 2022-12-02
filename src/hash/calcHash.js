523feff0d1783317c8a3ae21d7d9372c11fd4c41
ferka123 <ferka911@gmail.com>
2022-12-02 12:56:43 +0200

    feat: hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import crypto from "crypto";
+import fs from "fs/promises";
+import { createReadStream } from "fs";
+import { URL } from "url";
+
 const calculateHash = async () => {
-    // Write your code here 
+  return new Promise((resolve, reject) => {
+    try {
+      const src = new URL("./files/fileToCalculateHashFor.txt", import.meta.url)
+        .pathname;
+      const hash = crypto.createHash("sha256");
+
+      const input = createReadStream(src);
+      input.on("data", (chunk) => {
+        hash.update(chunk);
+      });
+      input.on("end", () => {
+        console.log(hash.digest("hex"));
+        resolve();
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
 };
 
 await calculateHash();
