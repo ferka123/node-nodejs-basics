0367209540c568d4443e39623780bae894d2c25a
ferka123 <ferka911@gmail.com>
2022-12-02 12:58:55 +0200

    feat: wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
+import { cpus } from "os";
+import { Worker, workerData } from "worker_threads";
+import { URL } from "url";
+
 const performCalculations = async () => {
-    // Write your code here
+  const threadsNum = cpus().length;
+  const workerPath = new URL("worker.js", import.meta.url).pathname;
+
+  const result = await Promise.all(
+    [...Array(threadsNum)].map((_, index) => {
+      return new Promise((resolve) => {
+        const worker = new Worker(workerPath, { workerData: 10 + index });
+        worker.once("message", (message) => {
+          resolve({ status: "resolved", data: message });
+        });
+        worker.once("error", () => {
+          resolve({ status: "error", data: null });
+        });
+      });
+    })
+  );
+
+  console.log(result);
 };
 
 await performCalculations();
