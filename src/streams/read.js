a5bea40e886d97695bf67d38697f2b471b51e2f9
ferka123 <ferka911@gmail.com>
2022-12-02 12:58:32 +0200

    feat: streams task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { createReadStream } from "fs";
+import { URL } from "url";
+
 const read = async () => {
-    // Write your code here 
+  return new Promise((resolve, reject) => {
+    try {
+      const path = new URL("./files/fileToRead.txt", import.meta.url).pathname;
+      const stream = createReadStream(path, "utf-8");
+      stream.pipe(process.stdout);
+      stream.on("end", resolve);
+    } catch (e) {
+      reject(e);
+    }
+  });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { createWriteStream } from "fs";
+import { URL } from "url";
+
 const write = async () => {
-    // Write your code here 
+  return new Promise((resolve) => {
+    const filePath = new URL("./files/fileToWrite.txt", import.meta.url)
+      .pathname;
+    const stream = createWriteStream(filePath);
+    process.stdin.pipe(stream);
+    process.on("SIGINT", resolve);
+  });
 };
 
 await write();
